13f6d0f7201a685de65a252d65c1ff137eb2a9ea
Carlos <carlos-eduardo-1995@live.com>
2018-10-22 21:53:27 -0500

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/GenerarOrdenCompraController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class GenerarOrdenCompraController : Controller
+    {
+        // GET: GenerarOrdenCompra
+        public ActionResult GenerarOrdenCompra()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GenerarOrdenCompra(PROVEEDOR proveedor)
+        {
+            var facade = new Facade();
+            int ruc = int.Parse(Request["txtProveedor"]);
+            var datos = facade.BuscarProveedor(ruc);
+            ViewData["RazonSocial"] = datos.RAZON_SOCIAL;
+            ViewData["Direccion"] = datos.DIRECCION;
+            ViewData["Ruc"] = datos.RUC;
+            ViewData["Telefono"] = datos.TELEFONO;
+            if (Request["txtDireccion"] != null, Request[""])
+            {
+
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/Facade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Facade
+    {
+        public PROVEEDOR BuscarProveedor(int RUC)
+        {
+            var context = new CARWASHEntities1();
+            var datos = context.PROVEEDOR.Find(RUC);
+            //context.SaveChanges();
+            return datos;
+        }
+        public MATERIALES BuscarMaterial(int RUC)
+        {
+            var context = new CARWASHEntities1();
+            var datos = context.MATERIALES.Find(RUC);
+            //context.SaveChanges();
+            return datos;
+        }
+    }
+}
